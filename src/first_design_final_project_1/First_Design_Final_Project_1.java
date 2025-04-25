014aa6c0c0136878a1ad75b3beafd29b2bb0e003
PC <PC@DESKTOP-8NV6ODQ>
2025-04-25 13:58:40 +0800

    Initial commit - uploading unfinished event manager java project

---
+++ src/first_design_final_project_1/First_Design_Final_Project_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first_design_final_project_1;
+
+/**
+ *
+ * @author PC
+ */
+public class First_Design_Final_Project_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
